9864de1d3c1b9cb344a58b771ab7ec19d0657bb6
Mark Waller <M_P_waller@hotmail.com>
2016-08-24 11:26:56 +0800

    Update Main.java

--- src/main/java/org/wallerlab/dso/Main.java
+++ src/main/java/org/wallerlab/dso/Main.java
@@ -3,10 +3,7 @@ package org.wallerlab.dso;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-
 import java.io.IOException;
-
-
 
 @SpringBootApplication
 public class Main {
